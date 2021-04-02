b38aff6a807dc533c5fd63a873a0a165593ef817
RdiegoSilva <77678402+RdiegoSilva@users.noreply.github.com>
2021-04-02 10:46:44 -0300

    Add files via upload

---
+++ bahasa/ind.js
@@ -0,0 +1,151 @@
+exports.wait = () => {
+	return`*⌛ TENHA PACIÊNCIA ESTOU PROCESSANDO SEU PEDIDO ⌛*`
+}
+
+exports.succes = () => {
+	return`〘 SUCESSO 〙`
+}
+
+exports.lvlon = () => {
+	return`〘 HABILITAR 〙 LEVELING`
+}
+
+exports.lvloff = () => {
+	return`〘 DESATIVAR 〙 LEVELING`
+}
+
+exports.lvlnul = () => {
+	return`*SEU NÍVEL AINDA ESTÁ VAZIO*`
+}
+
+exports.lvlnoon = () => {
+	return`*O NÍVEL DO GRUPO AINDA NÃO ESTÁ ATIVADO*`
+}
+
+exports.noregis = () => {
+	return`〘 🐼𝐑𝐄𝐆𝐈𝐒𝐓𝐑𝐄-𝐒𝐄 𝐏𝐑𝐈𝐌𝐄𝐈𝐑𝐎🐼 〙\n\n𝐂𝐎𝐌𝐎 𝐒𝐄-𝐑𝐄𝐆𝐈𝐒𝐓𝐀𝐑 ${prefix}\n𝐝𝐚𝐟𝐭𝐚𝐫 𝐍𝐨𝐦𝐞|𝐢𝐝𝐚𝐝𝐞 \n𝐄𝐱𝐞𝐦𝐩𝐥𝐨: ${prefix}𝐝𝐚𝐟𝐭𝐚𝐫 𝐏𝐚𝐧𝐝𝐚|𝟏𝟖`
+}
+
+exports.rediregis = () => {
+	return`〘 🐼𝐉𝐀 𝐑𝐄𝐆𝐈𝐒𝐓𝐑𝐀𝐃𝐎🐼 〙\n\n*Você já Está Registrado No Banco De Dados Do Bot Panda*`
+}
+
+exports.stikga = () => {
+	return`*Falhou tente repetir Novamente Em alguns momentos*`
+}
+
+exports.linkga = () => {
+	return`*Desculpe, o link é inválido*`
+}
+
+exports.groupo = () => {
+	return`〘 SÓ EM GRUPO 〙`
+}
+
+exports.ownerb = () => {
+	return`〘 PROPRIETÁRIO SOMENTE 〙`
+}
+
+exports.ownerg = () => {
+	return`〘 SOMENTE PROPRIETÁRIOS DO GRUPO 〙`
+}
+
+exports.admin = () => {
+	return`〘 SOMENTEADMINISTRAÇÃO DO GRUPO 〙`
+}
+
+exports.badmin = () => {
+	return`〘 O BOT DEVE SER ADMINISTRADOR 〙`
+}
+
+exports.nsfwoff = () => {
+	return`*NSFW ESTÁ DESLIGADO*`
+}
+
+exports.bug = () => {
+	return`*Problemas foram relatados ao proprietário Diego, relatórios falsos ou confusos não serão respondidos.*`
+}
+
+exports.wrongf = () => {
+	return`〘 Jovem Isso Pode Está Errado Reveja POR FAVOR! 〙*`
+}
+
+exports.clears = () => {
+	return`〘 CHAT LIMPO! 〙`
+}
+
+exports.pc = () => {
+	return`〘 CADASTRO 〙\n\nPara saber se você se cadastrou, verifique a mensagem que enviei \n\nNOTA:\n*Se você não entendeu a mensagem. significa que você não salvou o número do bot*`
+}
+
+exports.registered = (namaUser, umurUser, serialUser, time, sender) => {
+	return`〘 DADOS BOT PANDA 〙\n\nVocê Se Cadastrou Com Os Dados \n\n┏━➣Nome\n┗➣${namaUser}\n┏━➣Número\n┗➣wa.me/${sender.split("@")[0]}\n┏━➣idade\n┗➣${umurUser}\n┏━➣Hora Do Registro\n┗➣${time}\n\n┏━🐼 *BOT PANDA* 🐼━\n┣➣${serialUser}\n┗➣Nota : Pronto Voce Ja Foi Registrado, Para Usar O BOT Digite\n*${prefix}menu/help*`
+}
+
+exports.cmdnf = (prefix, command) => {
+	return`Comando *${prefix}${command}* Não encontrado \ Tente Escrever *${prefix}menu*`
+}
+
+exports.owneresce = (pushname) => {
+	return`*Desculpe, mas ${pushname} não e script do dono*`
+}
+
+exports.reglevelaha = (command, pushname, getLevelingLevel, sender, aha) => {
+	return`*Desculpe ${pushname} Seu nível não é suficiente*\n\n*┏⊱level mu : ${getLevelingLevel(sender)}*\n*┣⊱jenis command : ${command}*\n*┗⊱syarat level : ${aha}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
+}
+
+exports.reglevelahb = (command, pushname, getLevelingLevel, sender, ahb) => {
+	return`*Desculpe ${pushname} Seu nível não é suficiente*\n\n*┏⊱level mu : ${getLevelingLevel(sender)}*\n*┣⊱jenis command : ${command}*\n*┗⊱syarat level : ${ahb}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
+}
+
+exports.reglevelahc = (command, pushname, getLevelingLevel, sender, ahc) => {
+	return`*Desculpe ${pushname} Seu nível não é suficiente*\n\n*┏⊱level mu : ${getLevelingLevel(sender)}*\n*┣⊱jenis command : ${command}*\n*┗⊱syarat level : ${ahc}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
+}
+
+exports.reglevelahd = (command, pushname, getLevelingLevel, sender, ahd) => {
+	return`*Desculpe ${pushname} Seu nível não é suficiente*\n\n*┏⊱level mu : ${getLevelingLevel(sender)}*\n*┣⊱jenis command : ${command}*\n*┗⊱syarat level : ${ahd}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
+}
+
+exports.reglevelahe = (command, pushname, getLevelingLevel, sender, ahe) => {
+	return`*Desculpe ${pushname} Seu nível não é suficiente*\n\n*┏⊱level mu : ${getLevelingLevel(sender)}*\n*┣⊱jenis command : ${command}*\n*┗⊱syarat level : ${ahe}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
+}
+
+exports.reglevelahf = (command, pushname, getLevelingLevel, sender, ahf) => {
+	return`*Desculpe ${pushname} Seu nível não é suficiente*\n\n*┏⊱level mu : ${getLevelingLevel(sender)}*\n*┣⊱jenis command : ${command}*\n*┗⊱syarat level : ${ahf}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
+}
+
+exports.menu = (pushname, prefix, getLevelingLevel, getLevelingXp, sender, reqXp, _registered, uangku, role) => { 
+	return `
+`
+}
+
+exports.levelup = (pushname, sender, getLevelingXp,  getLevel, getLevelingLevel, role) => {
+	return`
+* FELIZ 〙
+ ➤ Nome : ${pushname}
+ ➤ Numero : ${sender.split("@")[0]}
+ ➤ Xp : ${getLevelingXp(sender)}
+ ➤ Limite :  +3
+ ➤ Pelotão :  ${role}
+ ➤ Level : ${getLevel} ⊱ ${getLevelingLevel(sender)}
+`}
+ 
+exports.limitend = (pushname) => {
+	return`*Desculpe ${pushname} O limite de hoje acabou*\n*Contate* : wa.me/558881060472\n\n*Nota : Daremos a você um aleatório de 1-10*`
+}
+
+exports.limitcount = (limitCounts) => {
+	return`
+〘 LIMITE CONTA 〙
+o resto do seu limite : ${limitCounts}
+
+NOTA : Para chegar ao limite. Você pode subir de nível por meio de grupos ou buylimit.
+`}
+
+exports.satukos = () => {
+	return`*Adicione o parâmetro 1 / ativar ou 0 / desativar`
+}
+
+exports.uangkau = (pushname, sender, uangkau) => {
+	return`◪ *〘 ATM 〙*\n  ├─ ❏ *Nome* : ${pushname}\n  ├─ ❏ *Numero* : ${sender.split("@")[0]}\n  └─ ❏ *Dinheiro* : ${uangkau}
+`}
